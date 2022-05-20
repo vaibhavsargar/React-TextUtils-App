a369768c9667b47e2c560bdaff049dcc0ccca182
Vaibhav <sargarvaibhav3@gmail.com>
2022-05-20 12:53:32 +0530

    practice start

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React practice1
         </a>
       </header>
     </div>
