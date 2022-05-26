78d8043c79e3a73a898154f0096f75f45d35cd6f
Vaibhav <sargarvaibhav3@gmail.com>
2022-05-26 18:04:11 +0530

    7: Basic UpperCase converter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 function App() {
-  let name = 'Vaibhav';
+  let name = "Vaibhav";
   return (
-    <div className="App">
-      <h3>hello {name}</h3>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React practice1.1
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title={"TextUtils"}/>
+      {/* <Navbar /> */}
+      <TextForm heading={"Enter the Text to Analyze"} />
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+    const handleUpClick = () => {
+        // console.log("handle was clicked: " + text);
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+    const handleOnChange = (event) => {
+        // console.log("Onchange");
+        setText(event.target.value);
+    }
+    
+    const [text, setText] = useState("Enter the text here");
+
+    return (
+        <div className="container my-3">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" />
+        </div>
+        <button className="btn btn-primary" onClick={handleUpClick}>
+            Convert to Uppercase
+        </button>
+        </div>
+  );
+}
