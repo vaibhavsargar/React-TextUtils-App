f0a00dc2d26eb8d1c656896eab4e27c154febb2f
Vaibhav <sargarvaibhav3@gmail.com>
2022-05-28 12:48:16 +0530

    10: about us with dark mode

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import About from "./components/About";
 
 function App() {
   let name = "Vaibhav";
@@ -10,6 +11,7 @@ function App() {
       <Navbar title={"TextUtils"}/>
       {/* <Navbar /> */}
       <TextForm heading={"Enter the Text to Analyze"} />
+      <About/>
     </>
   );
 }
