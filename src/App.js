e17f2845560c68991ac9a0488b9265667aa1bfaf
Vaibhav <sargarvaibhav3@gmail.com>
2022-05-30 17:11:22 +0530

    16: React router dom

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Alert from "./components/Alert";
 import TextForm from "./components/TextForm";
 import About from "./components/About";
 import {useState} from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 
 function App() {
@@ -47,15 +52,17 @@ function App() {
     }
   }
   
-
   return (
-    <>
+    <BrowserRouter>
       <Navbar title={"TextUtils"}  mode={mode} toggleMode={toggleMode}/>
       <Alert alert={alert} />
-      {/* <Navbar /> */}
-      <TextForm heading={"Enter the Text to Analyze"} value={5} mode={mode} alert={showAlert}/>
-      {/* <About/> */}
-    </>
+    <Routes>
+      <Route path="/" element={<TextForm heading={"Enter the Text to Analyze"} value={5} mode={mode} alert={showAlert}/>}>
+      </Route>
+      <Route path="/about" element={<About/>}>
+      </Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
